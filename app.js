72409f2b56236c37c042a36832540023d1632829
Suryansh Yadav <somuy24@gmail.com>
2022-07-01 19:23:50 +0530

    right url added 2

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dq9rpd5vl/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
